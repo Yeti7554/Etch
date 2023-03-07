1c106119f795ed745a0717ccde6575f27deaaae2
aasiddiq@ualberta.ca <abdullahsiddiqi@MacBook-Air.hitronhub.home>
2023-03-06 21:00:01 -0700

    Initialized a gird of divs with JS

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 // this is a great walk through
 // https://www.youtube.com/watch?v=dyhuaXeuyGo
+
+const board = document.querySelector(".board");
+
+// Create a 16 x 16 grid
+for (let i = 0; i < 16; i++) {
+    // Create a new row
+    const row = document.createElement("div");
+    row.classList.add("row");
+    
+    // Create 16 cells and append them to the row
+    for (let j = 0; j < 16; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      row.appendChild(cell);
+    }
+    
+    // Append the row to the board
+    board.appendChild(row);
+  }
