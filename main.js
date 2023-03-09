a79fb3fac3aa955a31a59931e965a28ec4222854
aasiddiq@ualberta.ca <abdullahsiddiqi@MacBook-Air.hitronhub.home>
2023-03-08 23:31:28 -0700

    Added cell dimension structure to JS

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // this is a great walk through
 // https://www.youtube.com/watch?v=dyhuaXeuyGo
 
+
+
 const board = document.querySelector(".board");
 
 // Create a 16 x 16 grid
@@ -8,14 +10,18 @@ for (let i = 0; i < 16; i++) {
     // Create a new row
     const row = document.createElement("div");
     row.classList.add("row");
+    row.style.height="20px";
     
     // Create 16 cells and append them to the row
     for (let j = 0; j < 16; j++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
+      cell.style.width="20px";
       row.appendChild(cell);
+
     }
     
     // Append the row to the board
     board.appendChild(row);
   }
+
