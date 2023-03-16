ae3fa509ca2b9fc50303424a4c83a43fe3808ee1
aasiddiq@ualberta.ca <abdullahsiddiqi@Abdullahs-Air.hitronhub.home>
2023-03-15 21:20:28 -0600

    added colour buttons

--- main.js
+++ main.js
@@ -5,21 +5,27 @@
 
 const board = document.querySelector(".board");
 
+let colour = 'black'
+
+function boxColor() {
+  this.style.backgroundColor= colour
+} // this is really useful
+
+
+
 // Create a 16 x 16 grid
 for (let i = 0; i < 16; i++) {
     // Create a new row
     const row = document.createElement("div");
     row.classList.add("row");
     row.style.height="20px";
-    
+
     // Create 16 cells and append them to the row
     for (let j = 0; j < 16; j++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
       cell.style.width="20px";
-      cell.addEventListener("mouseover", () =>{
-        cell.style.backgroundColor="black";
-      });
+      cell.addEventListener("mouseover", boxColor);
       row.appendChild(cell);
 
     }
@@ -28,8 +34,12 @@ for (let i = 0; i < 16; i++) {
     board.appendChild(row);
   }
 
-  
+
 
   // now need to add prompt for dimensions
   // variable that changes dimensions - though iterations and height and width
   // need to add event listener to change div colour
+
+  function changeColour(choice) {
+    colour = choice;
+  }
