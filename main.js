d0b4873c281e34495ba71981004d2d54a7737d98
Yeti7554clear <aasiddiq@ualberta.ca>
2023-03-18 12:31:37 -0600

    changed the function to create grid to using gridTemplateColumns and gridTemplateColumns

--- main.js
+++ main.js
@@ -2,60 +2,19 @@
 // https://www.youtube.com/watch?v=dyhuaXeuyGo
 
 
-
-const board = document.querySelector(".board");
-
-let colour = 'black'
-
-function boxColor() {
-  this.style.backgroundColor= colour
-} // this is really useful
+function populateBoard(size){
+  let board = document.querySelector('.board');
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`; // 16x16 grid
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 
-
-// Create a 16 x 16 grid
-for (let i = 0; i < 16; i++) {
-    // Create a new row
-    const row = document.createElement("div");
-    row.classList.add("row");
-    row.style.height="20px";
-
-    function resetBoard() {
-      row.style.backgroundColor="white";
-    };
-
-    // Create 16 cells and append them to the row
-    for (let j = 0; j < 16; j++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.style.width="20px";
-      cell.addEventListener("mouseover", boxColor);
-      row.appendChild(cell);
-
-      
-    }
-
-   
-
-    
-    // Append the row to the board
-    board.appendChild(row);
-
+  for(let i = 0; i<256; i++) {
+    let square = document.createElement('div');
+    square.style.backgroundColor = 'blue';
+    board.insertAdjacentElement("beforeend", square);
   }
 
-  function resetBoard() {
-    row.style.backgroundColor="white";
-  }
-  
+}
 
+populateBoard(16);
 
-
-  // now need to add prompt for dimensions
-  // variable that changes dimensions - though iterations and height and width
-  // need to add event listener to change div colour
-
-  function changeColour(choice) {
-    colour = choice;
-  }
-
- 
