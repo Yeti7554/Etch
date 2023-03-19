186c8031cc1f5e5c31ada1957bd96f92cb441f1c
Yeti7554clear <aasiddiq@ualberta.ca>
2023-03-19 13:52:16 -0600

    fixed all buttons and reset button

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 // this is a great walk through
 // https://www.youtube.com/watch?v=dyhuaXeuyGo
 
+let colour = 'black';
 
 function populateBoard(size){
   let board = document.querySelector('.board');
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`; // 16x16 grid
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-
-  for(let i = 0; i<256; i++) {
+  let amount = size * size; 
+  for(let i = 0; i< amount; i++) {
     let square = document.createElement('div');
-    square.style.backgroundColor = 'blue';
+    square.addEventListener('mouseover', colourSquare ); 
+    square.style.backgroundColor = 'white';
     board.insertAdjacentElement("beforeend", square);
   }
 
@@ -18,3 +22,20 @@ function populateBoard(size){
 
 populateBoard(16);
 
+function changeSize(input) {
+  populateBoard(input);
+}
+
+function colourSquare() {
+  this.style.backgroundColor = colour;
+}
+
+function changeColour(choice) {
+  colour = choice;
+}
+
+function resetBoard() {
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.style.backgroundColor = 'white');
+}
